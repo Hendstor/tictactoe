4dbd8d92c22ee480b752d2a87a6cc79d8056a605
Hendstor <venuefourteen@gmail.com>
2023-07-12 15:24:32 +0800

    first comming

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { StrictMode } from 'react';
+import { Fragment } from 'react';
+
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <Fragment>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </Fragment>
+  );
+}
+
